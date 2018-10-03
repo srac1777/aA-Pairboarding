11ea33138a799f06b46ec438709a1bcc9001c8ff
srac1777 <shashank.racherla@gmail.com>
2018-10-03 11:26:19 -0700

    finish uniqSubs

--- D4_A.js
+++ D4_A.js
@@ -1,2 +1,25 @@
 // Write a method that finds all the unique substrings for a word.
 
+const uniqSubs = (str) => {
+    let res = {};
+
+    for (let i = 0; i < str.length-1; i++) {
+        for (let j = i + 1; j < str.length; j++) {
+            if(!res[str.slice(i,j+1)]){
+                res[str.slice(i,j+1)] = true;
+            }
+        }
+    }
+    return Object.keys(res);
+}
+
+// console.log(uniqSubs("hellobilly"));
+
+// Given an array of integers(positive and negative)
+//  find the largest contiguous subsum(sum of a subarray).
+// You can solve this trivially in O(n ** 2) time by considering all subarrays.
+// Try to solve it in O(n) time with O(1) memory.
+
+const lcss = (arr) => {
+    
+}
